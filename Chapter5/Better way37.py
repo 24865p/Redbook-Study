ff7f5e5532e64284103cca3d747fc94b4bf88ac3
24865p <24865p@gmail.com>
2025-08-19 21:05:17 +0900

    5-38 완

--- Chapter5/Better way37.py
+++ Chapter5/Better way37.py
@@ -1,135 +1,135 @@
-# """
-# 파이썬은 내장 딕셔너리 타입을 사용하면 객체의 생명 주기 동안 동적인 내부 상태를 잘 유지할 수 있음
-# 동적: 어떤 값이 들어올지 미리 알 수 없는 식별자들을 유지
-# """
-# #
-# # 아이템 37
-# #
+"""
+파이썬은 내장 딕셔너리 타입을 사용하면 객체의 생명 주기 동안 동적인 내부 상태를 잘 유지할 수 있음
+동적: 어떤 값이 들어올지 미리 알 수 없는 식별자들을 유지
+"""
+#
+# 아이템 37
+#
 
 from collections import defaultdict
 
-# class SimpleGradebook:
-#     def __init__(self):
-#         self._grades = {}
+class SimpleGradebook:
+    def __init__(self):
+        self._grades = {}
 
-#     def add_student(self, name):
-#         self._grades[name] = []
+    def add_student(self, name):
+        self._grades[name] = []
 
-#     def report_grade(self, name, score):
-#         self._grades[name].append(score)
+    def report_grade(self, name, score):
+        self._grades[name].append(score)
 
-#     def average_grade(self, name):
-#         grades = self._grades[name]
-#         return sum(grades) / len(grades)
+    def average_grade(self, name):
+        grades = self._grades[name]
+        return sum(grades) / len(grades)
 
-# book = SimpleGradebook()
-# book.add_student('아이작 뉴턴')
-# book.report_grade('아이작 뉴턴', 90)
-# book.report_grade('아이작 뉴턴', 95)
-# book.report_grade('아이작 뉴턴', 85)
+book = SimpleGradebook()
+book.add_student('아이작 뉴턴')
+book.report_grade('아이작 뉴턴', 90)
+book.report_grade('아이작 뉴턴', 95)
+book.report_grade('아이작 뉴턴', 85)
 
-# print(book.average_grade('아이작 뉴턴'))
+print(book.average_grade('아이작 뉴턴'))
 
 
-# """
-# 딕셔너리와 관련 내장 타입은 사용하기 너무 쉬운만큼 과하게 확장하면서 깨지기 쉬움
-# """
-# #
+"""
+딕셔너리와 관련 내장 타입은 사용하기 너무 쉬운만큼 과하게 확장하면서 깨지기 쉬움
+"""
+#
 
-# class BySubjectGradebook:
-#     def __init__(self):
-#         self._grades = {}  # 외부 dict
+class BySubjectGradebook:
+    def __init__(self):
+        self._grades = {}  # 외부 dict
 
-#     def add_student(self, name):
-#         self._grades[name] = defaultdict(list)  # 내부 dict
+    def add_student(self, name):
+        self._grades[name] = defaultdict(list)  # 내부 dict
 
-#     def report_grade(self, name, subject, grade):
-#         by_subject = self._grades[name]
-#         grade_list = by_subject[subject]
-#         grade_list.append(grade)
+    def report_grade(self, name, subject, grade):
+        by_subject = self._grades[name]
+        grade_list = by_subject[subject]
+        grade_list.append(grade)
 
-#     def average_grade(self, name):
-#         by_subject = self._grades[name]
-#         total, count = 0, 0
-#         for grades in by_subject.values():
-#             total += sum(grades)
-#             count += len(grades)
-#         return total / count
+    def average_grade(self, name):
+        by_subject = self._grades[name]
+        total, count = 0, 0
+        for grades in by_subject.values():
+            total += sum(grades)
+            count += len(grades)
+        return total / count
 
-# book = BySubjectGradebook()
-# book.add_student('알버트 아인슈타인')
-# book.report_grade('알버트 아인슈타인', '수학', 70)
-# book.report_grade('알버트 아인슈타인', '수학', 65)
-# book.report_grade('알버트 아인슈타인', '체육', 90)
-# book.report_grade('알버트 아인슈타인', '체육', 95)
+book = BySubjectGradebook()
+book.add_student('알버트 아인슈타인')
+book.report_grade('알버트 아인슈타인', '수학', 70)
+book.report_grade('알버트 아인슈타인', '수학', 65)
+book.report_grade('알버트 아인슈타인', '체육', 90)
+book.report_grade('알버트 아인슈타인', '체육', 95)
+print(book.average_grade('알버트 아인슈타인'))
+
+#
+class WeightedGradebook:
+    def __init__(self):
+        self._grades = {}
+
+    def add_student(self, name):
+        self._grades[name] = defaultdict(list)
+
+    def report_grade(self, name, subject, score, weight):
+        by_subject = self._grades[name]
+        grade_list = by_subject[subject] # grade_list에 by_subject[subject]를 복사하는게 아님. "참조"로 같은 객체(self._grades)를 가리키고 있어서 grade_list에 append하면 self._grades가 영향을 받음
+        grade_list.append((score, weight))
+
+    def average_grade(self, name):
+        by_subject = self._grades[name]
+        score_sum, score_count = 0, 0
+
+        for subject, scores in by_subject.items():
+            subject_avg, total_weight = 0, 0
+
+            for score, weight in scores:
+                subject_avg += score * weight
+                total_weight += weight
+
+            score_sum += subject_avg / total_weight
+            score_count += 1
+
+        return score_sum / score_count
+
+
+book = WeightedGradebook()
+book.add_student('알버트 아인슈타인')
+book.add_student('남영')
+book.report_grade('남영', '수학', 95, 0.90)
+book.report_grade('알버트 아인슈타인', '수학', 75, 0.05)
+book.report_grade('알버트 아인슈타인', '수학', 65, 0.15)
+book.report_grade('알버트 아인슈타인', '수학', 70, 0.80)
+book.report_grade('알버트 아인슈타인', '체육', 100, 0.40)
+book.report_grade('알버트 아인슈타인', '체육', 85, 0.60)
 # print(book.average_grade('알버트 아인슈타인'))
+# print(book.average_grade('남영'))
 
-# #
-# class WeightedGradebook:
-#     def __init__(self):
-#         self._grades = {}
+"""
+위와 같이 복잡도가 눈에 들어오면 더 이상 딕셔너리, 튜플, 집합, 리스트 등의 내장 타입을 사용하지 말고 클래스 계층 구조를 사용해야 한다. 
+복잡도가 높은데 이중 이상으로 딕셔너리를 두면 유지 보수의 악몽 속으로 들어간다!!
+코드에서 값을 관리하는 부분이 점점 복잡해지고 있음을 깨달은 즉시 해당 기능을 클래스로 분리해야 한다.
+"""
 
-#     def add_student(self, name):
-#         self._grades[name] = defaultdict(list)
+grades = []
+grades.append((95, 0.45))
+grades.append((85, 0.55))
+total = sum(score * weight for score, weight in grades)
+total_weight = sum(weight for _, weight in grades)
+average_grade = total / total_weight
 
-#     def report_grade(self, name, subject, score, weight):
-#         by_subject = self._grades[name]
-#         grade_list = by_subject[subject] # grade_list에 by_subject[subject]를 복사하는게 아님. "참조"로 같은 객체(self._grades)를 가리키고 있어서 grade_list에 append하면 self._grades가 영향을 받음
-#         grade_list.append((score, weight))
+"""
+아래와 같이 튜플을 점점 더 길게 확장하는 패턴은 딕셔너리를 여러 단계로 내포시키는 경우와 유사함
+원소가 세 개 이상인 튜플을 사용한다면 다른 접근 방법을 생각해봐야 한다.
+"""
 
-#     def average_grade(self, name):
-#         by_subject = self._grades[name]
-#         score_sum, score_count = 0, 0
-
-#         for subject, scores in by_subject.items():
-#             subject_avg, total_weight = 0, 0
-
-#             for score, weight in scores:
-#                 subject_avg += score * weight
-#                 total_weight += weight
-
-#             score_sum += subject_avg / total_weight
-#             score_count += 1
-
-#         return score_sum / score_count
-
-
-# book = WeightedGradebook()
-# book.add_student('알버트 아인슈타인')
-# book.add_student('남영')
-# book.report_grade('남영', '수학', 95, 0.90)
-# book.report_grade('알버트 아인슈타인', '수학', 75, 0.05)
-# book.report_grade('알버트 아인슈타인', '수학', 65, 0.15)
-# book.report_grade('알버트 아인슈타인', '수학', 70, 0.80)
-# book.report_grade('알버트 아인슈타인', '체육', 100, 0.40)
-# book.report_grade('알버트 아인슈타인', '체육', 85, 0.60)
-# # print(book.average_grade('알버트 아인슈타인'))
-# # print(book.average_grade('남영'))
-
-# """
-# 위와 같이 복잡도가 눈에 들어오면 더 이상 딕셔너리, 튜플, 집합, 리스트 등의 내장 타입을 사용하지 말고 클래스 계층 구조를 사용해야 한다. 
-# 복잡도가 높은데 이중 이상으로 딕셔너리를 두면 유지 보수의 악몽 속으로 들어간다!!
-# 코드에서 값을 관리하는 부분이 점점 복잡해지고 있음을 깨달은 즉시 해당 기능을 클래스로 분리해야 한다.
-# """
-
-# grades = []
-# grades.append((95, 0.45))
-# grades.append((85, 0.55))
-# total = sum(score * weight for score, weight in grades)
-# total_weight = sum(weight for _, weight in grades)
-# average_grade = total / total_weight
-
-# """
-# 아래와 같이 튜플을 점점 더 길게 확장하는 패턴은 딕셔너리를 여러 단계로 내포시키는 경우와 유사함
-# 원소가 세 개 이상인 튜플을 사용한다면 다른 접근 방법을 생각해봐야 한다.
-# """
-
-# grades = []
-# grades.append((95, 0.45, '참 잘했어요'))
-# grades.append((85, 0.55, '조금 만 더 열심히'))
-# total = sum(score * weight for score, weight, _ in grades)
-# total_weight = sum(weight for _, weight, _ in grades)
-# average_grade = total / total_weight
+grades = []
+grades.append((95, 0.45, '참 잘했어요'))
+grades.append((85, 0.55, '조금 만 더 열심히'))
+total = sum(score * weight for score, weight, _ in grades)
+total_weight = sum(weight for _, weight, _ in grades)
+average_grade = total / total_weight
 
 #
 """
--- Chapter5/Better way38.py
+++ Chapter5/Better way38.py
@@ -1,15 +1,23 @@
 #
-# 아이템 38
+# 아이템 38 간단한 인터페이스의 경우 클래스 대신 함수를 받아라
 #
+
+"""
+- hook 함수: API가 실행되는 과정에서 내가 전달한 "함수"를 실행하는 경우
+- 인자와 반환 값이 잘 정의된, 상태가 없는 함수를 훅으로 사용하는 경우가 많음
+- 파이썬은 함수나 메서드를 "일급 시민 객체" 취급하므로 함수를 훅으로 사용할 수 있다.
+    -> 일반적인 값과 마찬가지로 다른 함수(메서드)에 넘기거나 변수로 참조할 수 있음
+"""
+from collections import defaultdict
+
 names = ['소크라테스', '아르키메데스', '플라톤', '아리스토텔레스']
-names.sort(key=len)
+names.sort(key=len) # key훅으로 len 내장 함수를 전달
 print(names)
 
-def log_missing():
+def log_missing(): # 정해진 동작과 side effect를 분리할 수 있어 API를 쉽게 만들 수 있음
     print('키 추가됨')
     return 0
 
-from collections import defaultdict
 current = {'초록': 12, '파랑': 3}
 increments = [
     ('빨강', 5),
@@ -22,7 +30,11 @@ for key, amount in increments:
     result[key] += amount
 print('이후:', dict(result))
 
-#
+"""
+defaultdict에 전달하는 디폴트 값 훅이 존재하지 않는 키에 접근한 총 횟수 세기
+-> 상태가 있는 클로저를 사용하면 됨
+-> 아래 코드는 클로저가 있는 도우미 함수를 디폴트 값 훅으로 사용함
+"""
 def increment_with_report(current, increments):
     added_count = 0
 
@@ -38,10 +50,12 @@ def increment_with_report(current, increments):
     return result, added_count
 
 result, count = increment_with_report(current, increments)
-assert count == 2
+assert count == 2 # defaultdict은 missing훅이 상태를 관리하는 것을 모르지만 함수를 실행하면 원하는 결과(2)를 볼 수 있음
+# 인터페이스에서 간단한 함수를 인자로 받으면 클로저 안에 상태를 감추는 기능 계층을 쉽게 추가할 수 있음 (하지만 이해하기 어려움)
 
 #
 class CountMissing:
+    """상태를 저장하는 작은 클래스 정의"""
     def __init__(self):
         self.added = 0
 
@@ -54,6 +68,14 @@ result = defaultdict(counter.missing, current) # 메서드 참조
 for key, amount in increments:
     result[key] += amount
 assert counter.added == 2
+
+"""
+더 깔끔하지만 CountMissing 클래스의 목적을 알기 어렵다
+- 명확한 표현을 위해 클래스에 __call__ 특별 메서드를 정의할 수 있음
+- __call__을 사용하면 객체를 함수처럼 사용할 수 있음
+- __call__이 정의된 클래스의 인스턴스에 대해 callable 내장 함수를 호출하면 다른 일반 함수나 메서드처럼 True가 반환됨
+- 이런 방식으로 호출되어 호출될 수 있는 모든 객체를 "callable" 객체라고 함
+"""
 
 #
 class BetterCountMissing:
@@ -69,11 +91,23 @@ counter = BetterCountMissing()
 assert counter() == 0
 assert callable(counter)
 
-#
+# BetterCountMissing 인스턴스를 defaultdict의 디폴트 값 훅으로 사용해서 존재하지 않는 키에 접근한 횟수를 추적
 counter = BetterCountMissing()
 result = defaultdict(counter, current) # __call__에 의존함
 for key, amount in increments:
     result[key] += amount
 assert counter.added == 2
+"""
+- __call__메서드는 API훅처럼 함수가 인자로 쓰일 수 있는 부분에 이 클래스의 인스턴스를 사용할수 있다는 사실을 나타냄
+- __call__부터 보고 이 클래스의 목적이 상태를 저장하는 클로저 역할이라는 것을 알 수 있음
+- defaultdict이 __call__ 내부에서 어떤 일이 벌어지는지 전혀 알 필요가 없음
+- defaultdict에게 필요한건 키가 없는 경우를 처리하기 위한 디폴트 값 훅 뿐임
+"""
 
-
+"""
+- 여러 컴포넌트 사이에 간단한 인터페이스가 필요할 때는 클래스를 정의하고 인스턴스화하는 대신 간단히 함수를 사용할 수 있음
+- 파이썬 함수나 메서드는 일급 시민이다.
+    -> 다른 타입의 값과 마찬가지로 함수나 함수 참조에 식을 사용할 수 있음
+- __call__ 특별 메서드를 사용하면 클래스의 인스턴스인 객체를 일반 파이썬 함수처럼 호출할 수 있다.
+- 상태를 유지하기 위한 함수가 필요한 경우 상태가 있는 클로저 정의 대신 __call__메서드가 있는 클래스를 정의할 지 고려해보자
+"""
